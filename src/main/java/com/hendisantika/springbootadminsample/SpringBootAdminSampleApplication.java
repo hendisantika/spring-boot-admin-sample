eb449d25b8e3f93a11d8f23ee07f17c0c4f3a39b
Hendi Santika <hendisantika@gmail.com>
2018-07-30 04:01:33 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootadminsample/SpringBootAdminSampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootadminsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminSampleApplication.class, args);
+    }
+}
